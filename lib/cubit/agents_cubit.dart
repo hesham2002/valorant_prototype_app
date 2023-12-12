b6499e32af4ebc80608beef5c8c53fcba0c5e9eb
hesham2002 <heshamgharib2002@gmail.com>
2023-12-12 03:29:04 +0200

    finishing cubit state management (agents_state.dart)

--- lib/cubit/agents_cubit.dart
+++ lib/cubit/agents_cubit.dart
@@ -1,8 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../data/model/character_model.dart';
+
 part 'agents_state.dart';
 
 class AgentsCubit extends Cubit<AgentsState> {
-  AgentsCubit() : super(AgentsInitial());
+  AgentsCubit() : super(AgentsInitialState());
 }
--- lib/cubit/agents_state.dart
+++ lib/cubit/agents_state.dart
@@ -3,4 +3,46 @@ part of 'agents_cubit.dart';
 @immutable
 abstract class AgentsState {}
 
-class AgentsInitial extends AgentsState {}
+class AgentsInitialState extends AgentsState {
+  AgentsInitialState();
+}
+
+class AgentsLoadingState extends AgentsState {
+  AgentsLoadingState();
+}
+
+class AgentsSuccessState extends AgentsState {
+  final List<CharacterModel> charList;
+
+  AgentsSuccessState({required this.charList});
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+
+    return other is AgentsSuccessState && other.charList == charList;
+  }
+
+  @override
+  int get hashCode => charList.hashCode;
+}
+
+class AgentsFailureState extends AgentsState {
+  final String message;
+
+  AgentsFailureState({required this.message});
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+
+    return other is AgentsFailureState && other.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
+}
